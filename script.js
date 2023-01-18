6dd211800633dfa0dc32b4d7e4e697beee01573e
Bryan <bryancabreram3@gmail.com>
2023-01-18 10:29:15 -0300

    Modificando map

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const objetoRegiones = [
-    { nombre: 'Arica y Parinacota', atractivos: 'Morro de Arica y lago Chungará', img: 'img/arica.jpg', id: 1 },
-    { nombre: 'Tarapacá', atractivos: 'Salitreras Humberstone y  Santa Laura', img: 'img/tarapaca.jpg', id: 2 },
-    { nombre: 'Antofagasta', atractivos: 'Valle de la Luna y lagunas antiplánicas', img: 'img/antofagasta.jpg', id: 3 },
-    { nombre: 'Atacama', atractivos: 'Bahía Inglesa y Caldera', img: 'img/atacama.jpg', id: 4 },
-    { nombre: 'Coquimbo', atractivos: 'Cruz del Tercer Milenio y Fuerte Lambert', img: 'img/coquimbo.jpg', id: 5 },
-    { nombre: 'Valparaíso', atractivos: 'Cerro artillería y Cerro Alegre', img: 'img/valparaiso.jpg', id: 6 },
-    { nombre: 'Metropolitana', atractivos: 'Costanera Center', img: 'img/metropolitana.jpg', id: 7 },
-    { nombre: "O'Higgins", atractivos: '', img: 'img/ohiggins.jpg', id: 8 },
-    { nombre: 'Maule', atractivos: 'Altos de Lircay y Las Siete Tazas', img: 'img/maule.jpg', id: 9 },
-    { nombre: 'Ñuble', atractivos: 'Valle de Itata', img: 'img/ñuble.jpg', id: 10 },
-    { nombre: 'Biobío', atractivos: 'Salto del Laja', img: 'img/biobio.jpg', id: 11 },
-    { nombre: 'Araucanía', atractivos: 'Huerquehue y Conguillío', img: 'img/araucania.jpg', id: 12 },
-    { nombre: 'Ríos', atractivos: 'Rio Calle Calle y Fuerte Niebla', img: 'img/los-rios.jpg', id: 13 },
-    { nombre: 'Lagos', atractivos: 'Chiloé y Frutillar', img: 'img/los-lagos.jpg', id: 14 },
-    { nombre: 'Aysén', atractivos: 'Laguna San Rafael y Queulat', img: 'img/aysen.jpg', id: 15 },
-    { nombre: 'Magallanes', atractivos: 'Torres del Paine y la Antártica', img: 'img/magallanes.jpg', id: 16 }
+    { nombre: 'Arica y Parinacota', atractivos: 'Morro de Arica y lago Chungará', img: 'img/arica.jpg', id: 1},
+    { nombre: 'Tarapacá', atractivos: 'Salitreras Humberstone y  Santa Laura', img: 'img/tarapaca.jpg', id: 2},
+    { nombre: 'Antofagasta', atractivos: 'Valle de la Luna y lagunas antiplánicas', img: 'img/antofagasta.jpg', id: 3},
+    { nombre: 'Atacama', atractivos: 'Bahía Inglesa y Caldera', img: 'img/atacama.jpg', id: 4},
+    { nombre: 'Coquimbo', atractivos: 'Cruz del Tercer Milenio y Fuerte Lambert', img: 'img/coquimbo.jpg', id: 5},
+    { nombre: 'Valparaíso', atractivos: 'Cerro artillería y Cerro Alegre', img: 'img/valparaiso.jpg', id: 6},
+    { nombre: 'Metropolitana', atractivos: 'Costanera Center', img: 'img/metropolitana.jpg', id: 7},
+    { nombre: "O'Higgins", atractivos: '', img: 'img/ohiggins.jpg', id: 8},
+    { nombre: 'Maule', atractivos: 'Altos de Lircay y Las Siete Tazas', img: 'img/maule.jpg', id: 9},
+    { nombre: 'Ñuble', atractivos: 'Valle de Itata', img: 'img/ñuble.jpg', id: 10},
+    { nombre: 'Biobío', atractivos: 'Salto del Laja', img: 'img/biobio.jpg', id: 11},
+    { nombre: 'Araucanía', atractivos: 'Huerquehue y Conguillío', img: 'img/araucania.jpg', id: 12},
+    { nombre: 'Ríos', atractivos: 'Rio Calle Calle y Fuerte Niebla', img: 'img/los-rios.jpg', id: 13},
+    { nombre: 'Lagos', atractivos: 'Chiloé y Frutillar', img: 'img/los-lagos.jpg', id: 14},
+    { nombre: 'Aysén', atractivos: 'Laguna San Rafael y Queulat', img: 'img/aysen.jpg', id: 15},
+    { nombre: 'Magallanes', atractivos: 'Torres del Paine y la Antártica', img: 'img/magallanes.jpg', id: 16}
 ]
 function mostrarInfo(id) {
     switch (id) {
